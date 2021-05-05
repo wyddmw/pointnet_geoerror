ac0cfad6da5fd3cf7bf23c9f36533f824c87593c
spyder <zhangsongyan@CN3614000070M.local>
2021-05-05 14:11:52 +0800

    add some update

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train_classification.py --dataset /home/spyder/hazel/shapenet_dataset/shapenetcore_partanno_segmentation_benchmark_v0/ --nepoch=5 --dataset_type shapenet --batchSize 4
+python train_classification_geoerror.py --data_path  --nepoch=5  --batchSize 4
