c27f6ba655dda28f2855c93bd6f593786600fe52
spyder <zhangsongyan@CN3614000070M.local>
2021-05-05 16:07:08 +0800

    add some update

--- model/model.py
+++ model/model.py
@@ -38,7 +38,7 @@ class STN3d(nn.Module):
         x = F.relu(self.bn5(self.fc2(x)))
         x = self.fc3(x)
 
-        iden = Variable(torch.from_numpy(np.array([1,0,0,0,1,0,0,0,1]).astype(np.float32))).view(1,9).repeat(batchsize,1)
+        iden = Variable(torch.from_numpy(np.array([1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1]).astype(np.float32))).view(1,16).repeat(batchsize,1)
         if x.is_cuda:
             iden = iden.cuda()
         x = x + iden
